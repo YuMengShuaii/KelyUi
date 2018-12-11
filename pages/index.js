3de154ea5ddcca196c57550fa103771ec7acc44c
yumengshuai <git yumengshuai@gmail.com>
2018-12-12 00:39:57 +0800

    '初始化项目'

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export default class Index extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (<div>
+         <p>我的第一个Next程序!</p>
+        </div>)
+    }
+}
